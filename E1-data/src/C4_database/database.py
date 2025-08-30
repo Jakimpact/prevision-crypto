9164e835f00ebe0f1d533737f15cf2b1bc3a0b42
Quentin <jakimpact@gmail.com>
2025-08-30 15:57:55 +0200

    chore(E1): modification import variables d'environnements

--- E1-data/src/C4_database/database.py
+++ E1-data/src/C4_database/database.py
@@ -19,7 +19,7 @@ from src.C4_database.crud import (
     ForecastDailyCRUD
 )
 from src.C4_database.models import Base
-from src.settings import DatabaseSettings, SecretSettings
+from src.settings import SecretSettings
 
 db_username = SecretSettings.DB_USERNAME
 db_password = SecretSettings.DB_PASSWORD
@@ -27,6 +27,10 @@ db_host = SecretSettings.DB_HOST
 db_port = SecretSettings.DB_PORT
 db_name = SecretSettings.DB_NAME
 
+
+# Vérification pour s'assurer que les variables ne sont pas None avant de construire l'URL
+if not all([db_username, db_password, db_host, db_port, db_name]):
+    raise ValueError("Certaines informations de connexion à la base de données sont manquantes. Vérifiez vos variables d'environnement.")
 
 engine = create_engine(
     f"postgresql+psycopg2://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
